8cc9f3c10e70b4dc05536693fb46b9aab24982b8
Johanna Hultberg <Johanna@us-198-167.eduroam.liu.se>
2016-02-23 18:22:23 +0100

    klar med lab 4 fortsätter med bilder

--- Assignment 4/Lab4.m
+++ Assignment 4/Lab4.m
@@ -8,54 +8,50 @@ map = 1;
 gwinit(map);
 state = gwstate;
 
-gamma = 0.99;
+gamma = 0.5;
 alpha = 0.3;
+epsilon=0.9;
 
-% as long as start position is not allowed generate a new one
-while state.posval(state.pos(1), state.pos(2))<-1
-    gwinit(map);
-    state = gwstate;
-end
-Q = rand(state.xsize, state.ysize, nbrActions)*(-1)-0.1;  % Look-up table
-Q(1,:,2)=-inf; %edges
-%Q(state.xsize,:,1)=-inf;
+% Look-up table
+Q = rand(state.xsize, state.ysize, nbrActions);
+%edges
+Q(1,:,2)=-inf;
+Q(state.xsize,:,1)=-inf;
 Q(:,1,4)=-inf;
-%Q(:,state.ysize,3)=-inf;
+Q(:,state.ysize,3)=-inf;
 gwdraw
 decisionVector = [];
-numIterations = 500;
+numIterations = 1000;
+numstate=zeros(numIterations,1);
+
 for episodes = 1:numIterations
+    episodes
     % gives random start position
     gwinit(map);
     state = gwstate;
-    epsilon=episodes/numIterations;
-    % as long as start position is not allowed generate a new one
-    while state.posval(state.pos(1), state.pos(2))<-1
-        gwinit(map);
-        state = gwstate;
-    end
+    epsilon=epsilon-epsilon*episodes/numIterations;
     
     % repeat until goal found
     while (state.isterminal ~=1 )
         % chose an action
-        if(rand(1)>epsilon)
+        if(rand(1)<epsilon)
             action = ceil(rand(1)*4);
-            if Q(state.pos(1), state.pos(2), action) == -inf
-                [~,action]=max(Q(state.pos(1), state.pos(2),:));
+            while Q(state.pos(1), state.pos(2), action) == -inf
+                action = ceil(rand(1)*4);
             end
         else
             [~,action]=max(Q(state.pos(1), state.pos(2),:));
         end
         oldstate=gwstate;
         state=gwaction(action);
+        numstate(episodes)=numstate(episodes)+1;
         reward=oldstate.feedback;
         Q(oldstate.pos(1),oldstate.pos(2), action)=...
             (1-alpha)*Q(oldstate.pos(1),oldstate.pos(2),action)+...
-            alpha*(reward+gamma*max(Q(state.pos(1), state.pos(2),action)));
+            alpha*(reward+gamma*max(Q(state.pos(1), state.pos(2),:)));
     end
-    
+    Q(state.pos(1),state.pos(2),:) = 0;
 end
-%gwdraw
 
 %% plot decisions
 [~,bestdec]=max(Q,[],3);
@@ -66,3 +62,12 @@ for x=1:state.xsize
         gwplotarrow([x,y]',bestdec(x,y));
     end
 end
+figure(5)
+imagesc(max(Q,[],3))
+title('max(Q) for map 3')
+colorbar
+
+%% plot convergence
+figure(6)
+plot(numstate)
+title('Plot for convergation')
